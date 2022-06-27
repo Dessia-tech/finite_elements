9c5ea247c5841929f98a0e7bab38d669363a1174
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-06-27 09:36:56 +0200

    finite_element_poutre.py (update)

--- scripts/finite_element_poutre.py
+++ scripts/finite_element_poutre.py
@@ -17,24 +17,35 @@ phase3 = 3
 mu1 = 4*math.pi*1e-7 * 100000 # IRON
 mu2 = 4*math.pi*1e-7          # VOID
 mu3 = 4*math.pi*1e-7 * 50000  # IRON
+
 elements_phase1 = []
 for i in range(phase1):
     element1 = mesh.TriangularElement2D([vm.Point2D(i,0), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     element2 = mesh.TriangularElement2D([vm.Point2D(i+1,1), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     elements_phase1.extend([element1, element2])
-group_phase1 = fe.elements.MagneticElementsGroup(elements_phase1, mu1, 'phase1')
+
+magnetic_elements = [fe.elements.MagneticElement2D(element, mu1) for element in elements_phase1]
+group_phase1 = fe.elements.MagneticElementsGroup(magnetic_elements, mu1, 'phase1')
+
 elements_phase2 = []
 for i in range(phase1, phase1+phase2):
     element1 = mesh.TriangularElement2D([vm.Point2D(i,0), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     element2 = mesh.TriangularElement2D([vm.Point2D(i+1,1), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     elements_phase2.extend([element1, element2])
-group_phase2 = fe.elements.MagneticElementsGroup(elements_phase2, mu2, 'phase2')
+
+magnetic_elements = [fe.elements.MagneticElement2D(element, mu2) for element in elements_phase2]
+group_phase2 = fe.elements.MagneticElementsGroup(magnetic_elements, mu2, 'phase2')
+
+
 elements_phase3 = []
 for i in range(phase1+phase2, phase1+phase2+phase3):
     element1 = mesh.TriangularElement2D([vm.Point2D(i,0), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     element2 = mesh.TriangularElement2D([vm.Point2D(i+1,1), vm.Point2D(i+1,0), vm.Point2D(i,1)])
     elements_phase3.extend([element1, element2])
-group_phase3 = fe.elements.MagneticElementsGroup(elements_phase3, mu3, 'phase3')
+
+magnetic_elements = [fe.elements.MagneticElement2D(element, mu3) for element in elements_phase3]
+group_phase3 = fe.elements.MagneticElementsGroup(magnetic_elements, mu3, 'phase3')
+
 mesh = mesh.Mesh([group_phase1, group_phase2, group_phase3])
 elements_loads = [fe.loads.ConstantLoad(group_phase1.elements[0:2], 1e10)]
 node_loads = []
