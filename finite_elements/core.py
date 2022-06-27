c39e0d38790811c5f914c4ca58f568f3a883339a
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-06-27 13:48:09 +0200

    global_matrix_positions

--- finite_elements/core.py
+++ finite_elements/core.py
@@ -29,3 +29,28 @@ cdict = {'red':  [(0.0, 0.0, 0.0),
 blue_red = LinearSegmentedColormap('BLueRed', cdict)
 
 MU = 4*math.pi*1e-7
+
+def global_matrix_positions(dimension, nodes_number):
+
+    if dimension == 2:
+        return global_matrix_positions2d(nodes_number)
+    if dimension == 3:
+        return global_matrix_positions3d(nodes_number)
+
+def global_matrix_positions2d(nodes_number):
+
+    positions = {}
+    count = 0
+    for i in range(nodes_number):
+        positions[i] = {'x':count, 'y':count+1, 'z':count+2}
+        count +=3
+    return positions
+
+def global_matrix_positions3d(nodes_number):
+
+    positions = {}
+    count = 0
+    for i in range(nodes_number):
+        positions[i] = {'x':count, 'y':count+1}
+        count +=2
+    return positions
