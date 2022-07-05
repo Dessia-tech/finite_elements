e771311d5a3c22ced77d0f05aa31f99064b6a987
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-07-05 15:40:39 +0200

    solid_mechanics_tetrahedral_element3d_example_1.py

--- scripts/solid_mechanics_tetrahedral_element3d_example_1.py
+++ scripts/solid_mechanics_tetrahedral_element3d_example_1.py
@@ -14,18 +14,24 @@ import finite_elements.loads
 import finite_elements.analysis
 import finite_elements.conditions
 
-# %% Mesh2D
+# %% Mesh3D
 
 elasticity_modulus, poisson_ratio = 96, 1/3
 
 points = [vm.Point3D(2,3,4), vm.Point3D(6,3,2), vm.Point3D(2,5,1), vm.Point3D(4,3,6)]
 
-solid_elment3d = fe.elements.SolidMechanicsTetrahedralElement3D(
+solid_elments3d = [fe.elements.SolidMechanicsTetrahedralElement3D(
     mesh_element = mesh.TetrahedralElement(points),
     elasticity_modulus = elasticity_modulus,
-    poisson_ratio = poisson_ratio)
+    poisson_ratio = poisson_ratio)]
 
-stiffness_matrix = solid_elment3d.elementary_matrix()
+group_solid_elments3d = mesh.ElementsGroup(solid_elments3d, '')
+mesh = mesh.Mesh([group_solid_elments3d])
+
+stiffness_matrix = solid_elments3d[0].elementary_matrix()
+
+
+# %% Analysis
 
 analysis = fe.analysis.FiniteElementAnalysis(mesh, [], [], [], [], [], [])
 
