dd272e674a9bc03428f055e16eebb9a31e98db86
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-07-05 11:36:13 +0200

    update

--- scripts/solid_mechanics_tetrahedral_element3d_example_1.py
+++ scripts/solid_mechanics_tetrahedral_element3d_example_1.py
@@ -26,36 +26,3 @@ solid_elment3d = fe.elements.SolidMechanicsTetrahedralElement3D(
     poisson_ratio = poisson_ratio)
 
 stiffness_matrix = solid_elment3d.elementary_matrix()
-
-# points = [vm.Point3D(1,1,0), vm.Point3D(-4,3,6), vm.Point3D(-1,0,3), vm.Point3D(2,4,-5)]
-
-# triangles = [mesh.TriangularElement2D([vm.Point2D(3,0),vm.Point2D(3,2),vm.Point2D(0,0)]),
-#              mesh.TriangularElement2D([vm.Point2D(0,2),vm.Point2D(0,0),vm.Point2D(3,2)])]
-
-# solid_elments2d = [fe.elements.SolidMechanicsTriangularElement2D(
-#     triangle, elasticity_modulus, poisson_ratio, thickness) for triangle in triangles]
-
-# group_solid_elments2d = mesh.ElementsGroup(solid_elments2d, '')
-# mesh = mesh.Mesh([group_solid_elments2d])
-
-# # group_solid_elments2d = [mesh.ElementsGroup([solid_elment], '') for solid_elment in solid_elments2d]
-# # mesh = mesh.Mesh(group_solid_elments2d)
-
-# mesh.node_to_index[mesh.nodes[3]] = 2
-# mesh.node_to_index[mesh.nodes[2]] = 3
-
-# # %%
-
-# node_loads = [fe.loads.SingleNodeLoad(mesh.nodes[1], -1000, 2)]
-# node_boundary_conditions = [finite_elements.conditions.NodeBoundaryCondition(vm.Point2D(3,0), 0, 2),
-#                             finite_elements.conditions.NodeBoundaryCondition(vm.Point2D(0,2), 0, 1),
-#                             finite_elements.conditions.NodeBoundaryCondition(vm.Point2D(0,2), 0, 2),
-#                             finite_elements.conditions.NodeBoundaryCondition(vm.Point2D(0,0), 0, 1),
-#                             finite_elements.conditions.NodeBoundaryCondition(vm.Point2D(0,0), 0, 2)]
-
-# analysis = fe.analysis.FiniteElementAnalysis(mesh, [], node_loads, [], [], node_boundary_conditions, [])
-
-# m = analysis.create_matrix()
-
-# results = analysis.solve()
-
