a4d2b6153d28e13f03e90d60231433ee37f206a8
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-07-12 12:16:56 +0200

    ElasticityResults: plots (update)

--- finite_elements/results.py
+++ finite_elements/results.py
@@ -563,47 +563,51 @@ class ElasticityResults(Result):
         return self.plot_constraints(constraint_name='shear_stress_xy', ax=ax, fig=fig)
 
     def plot_strain(self, axs=None, fig=None, row=1):
-        if fig is None:
-            fig = plt.figure()
-        # if axs is None:
-        #     fig, axs = plt.subplots(1, 3)
+        # if fig is None:
+        #     fig = plt.figure()
+        if axs is None:
+            fig, axs = plt.subplots(1, 3)
 
         plot_names = ['plot_axial_strain_x', 'plot_axial_strain_y', 'plot_shear_strain_xy']
-        axs = []
-        for i, name in enumerate(plot_names):
-            axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
+        # axs = []
+        # for i, name in enumerate(plot_names):
+        #     axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
 
-        # for i, ax in enumerate(axs.ravel()):
-        #     ax = getattr(self, plot_names[i])(ax=ax, fig=fig)
+        for i, ax in enumerate(axs.ravel()):
+            ax = getattr(self, plot_names[i])(ax=ax, fig=fig)
 
         return axs
 
     def plot_stress(self, axs=None, fig=None, row=1):
-        if fig is None:
-            fig = plt.figure()
-        # if axs is None:
-        #     fig, axs = plt.subplots(1, 3)
+        # if fig is None:
+        #     fig = plt.figure()
+        if axs is None:
+            fig, axs = plt.subplots(1, 3)
 
         plot_names = ['plot_axial_stress_x', 'plot_axial_stress_y', 'plot_shear_stress_xy']
-        axs = []
-        for i, name in enumerate(plot_names):
-            axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
+        # axs = []
+        # for i, name in enumerate(plot_names):
+        #     axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
 
-        # for i, ax in enumerate(axs.ravel()):
-        #     ax = getattr(self, plot_names[i])(ax=ax, fig=fig)
+        for i, ax in enumerate(axs.ravel()):
+            ax = getattr(self, plot_names[i])(ax=ax, fig=fig)
 
         return axs
 
     def plot_strain_stress(self, axs=None, fig=None):
-        # if axs is None:
-        #     fig, axs = plt.subplots(2, 3)
-        if fig is None:
-            fig = plt.figure()
+        if axs is None:
+            fig, axs = plt.subplots(2, 3)
+        # if fig is None:
+        #     fig = plt.figure()
+
+        axs = axs.ravel()
+        self.plot_strain(axs=axs[0:3], fig=fig)
+        self.plot_stress(axs=axs[3::], fig=fig)
 
         # self.plot_strain(axs=axs[0, 0:3], fig=fig)
         # self.plot_stress(axs=axs[1, 0:3], fig=fig)
 
-        self.plot_strain(axs=axs, fig=fig, row=2)
-        self.plot_stress(axs=axs, fig=fig, row=2)
+        # self.plot_strain(axs=axs, fig=fig, row=2)
+        # self.plot_stress(axs=axs, fig=fig, row=2)
 
         return axs
