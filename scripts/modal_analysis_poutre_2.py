2f83d7ed7bfe482c177c43b751f892ee5e014cd0
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-07-15 13:33:01 +0200

    scripts/modal_analysis_poutre_2.py

---
+++ scripts/modal_analysis_poutre_2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 15 2022
+
+@author: b.soumiya
+"""
+
+import volmdlr.gmsh
+import volmdlr.mesh as vmmesh
+import finite_elements as fe
+import finite_elements.elements
+import finite_elements.loads
+import finite_elements.analysis
+import finite_elements.conditions
+
+# %% Mesh2D
+
+file_path = 'mesh_2.msh'
+
+gmsh = volmdlr.gmsh.Gmsh.from_file(file_path)
+
+mesh = gmsh.define_triangular_element_mesh()
+
+mesh.plot()
+
+# %% Finite Element Mesh2D
+
+# elasticity_modulus, poisson_ratio, thickness, mass_density = 70*1e6, 0.33, 1, 2700 #aluminium
+elasticity_modulus, poisson_ratio, thickness, mass_density = 45*1e6, 0.29, 1, 1800 #magnesium
+group_elements = []
+
+for group in mesh.elements_groups:
+    solid_elments2d = []
+    for triangle in group.elements:
+        solid_elments2d.append(fe.elements.ElasticityTriangularElement2D(
+            triangle, elasticity_modulus, poisson_ratio, mass_density, thickness))
+
+    group_elements.append(vmmesh.ElementsGroup(solid_elments2d, ''))
+
+mesh = vmmesh.Mesh(group_elements)
+
+
+# %% Analysis
+
+analysis = fe.analysis.FiniteElementAnalysis(mesh, [], [], [], [], [], [])
+
+eigvals, eigvecs = analysis.modal_analysis()
+elasticity_results = []
+
+for eigvec in eigvecs:
+    elasticity_results.append(fe.results.ElasticityResults2D(analysis.mesh,
+                                                             eigvec))
+
+for elasticity_result in elasticity_results[0:6]:
+    elasticity_result.plot_deformed_mesh()
+
