d9be52196690d17d5d7d2ceeddb3485257dcbedb
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-07-29 12:02:04 +0200

    scripts/Elasticity/poutre2d_example_2.py (update)

--- scripts/Elasticity/poutre2d_example_2.py
+++ scripts/Elasticity/poutre2d_example_2.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 18 2022
+Created on Fri Jul 29 2022
 
 @author: b.soumiya
 """
 
-import volmdlr.gmsh
 import volmdlr as vm
-import volmdlr.mesh as vmmesh
+import volmdlr.mesh as mesh
 import finite_elements as fe
 import finite_elements.elements
 import finite_elements.loads
@@ -31,90 +30,86 @@ def find_points_with_y(nodes, y):
 
 # %% Mesh2D
 
-file_path = 'mesh_3.msh'
-
-gmsh = volmdlr.gmsh.Gmsh.from_file(file_path)
-
-mesh = gmsh.define_triangular_element_mesh()
-
-mesh.plot()
-
-# # %% Finite Element Mesh2D
-
-# elasticity_modulus, poisson_ratio, thickness, mass_density = 30*1e6, 0.25, 0.5, 2.7
-# # elasticity_modulus, poisson_ratio, thickness, mass_density = 70*1e3, 0.25, 0.5, 2.7
-
-# group_elements = []
-
-# for group in mesh.elements_groups:
-#     solid_elments2d = []
-#     for triangle in group.elements:
-#         solid_elments2d.append(fe.elements.ElasticityTriangularElement2D(
-#             triangle, elasticity_modulus, poisson_ratio, mass_density, thickness))
-
-#     group_elements.append(vmmesh.ElementsGroup(solid_elments2d, ''))
-
-# mesh = vmmesh.Mesh(group_elements)
-
-# # %% Loads/Conditions
-
-# load = -1000
-# application_index = mesh.node_to_index[volmdlr.mesh.Node2D(10,1)]
-# node_loads = [fe.loads.NodeLoad(mesh.nodes[application_index], load, 2)]
-
-# application_indices = find_points_with_x(nodes=mesh.nodes, x=0)
-# node_boundary_conditions = []
-# for index in application_indices:
-#     node_boundary_conditions.extend([finite_elements.conditions.NodeBoundaryCondition(mesh.nodes[index], 0, 1),
-#                                 finite_elements.conditions.NodeBoundaryCondition(mesh.nodes[index], 0, 2)])
-
-# #%% Analysis: plane_strain
-
-# analysis = fe.analysis.FiniteElementAnalysis(mesh, [], node_loads, [], [], node_boundary_conditions, [],
-#                                              plane_strain=True, plane_stress=False)
-
-# m = analysis.create_matrix()
-
-# results = analysis.solve()
-
-# elasticity_result = fe.results.ElasticityResults2D(results.mesh, results.result_vector, analysis.plane_strain, analysis.plane_stress)
-
-# # elasticity_result.plot_deformed_mesh()
-# # elasticity_result.plot_displacement_vectors_per_node()
-
-# # # elasticity_result.plot_strain()
-# # # elasticity_result.plot_stress()
-
-# # # elasticity_result.plot_displacement_per_node_x()
-# # # elasticity_result.plot_displacement_per_node_y()
-# # elasticity_result.plot_displacement_per_node_xy()
-
-# # #%% Analysis: plane_stress
-
-# # analysis = fe.analysis.FiniteElementAnalysis(mesh, [], node_loads, [], [], node_boundary_conditions, [],
-# #                                              plane_strain=False, plane_stress=True)
-
-# # m = analysis.create_matrix()
-
-# # results = analysis.solve()
-
-# # elasticity_result = fe.results.ElasticityResults2D(results.mesh, results.result_vector, analysis.plane_strain, analysis.plane_stress)
-
-# # elasticity_result.plot_deformed_mesh()
-# # elasticity_result.plot_displacement_vectors_per_node()
-
-# # # elasticity_result.plot_strain()
-# # # elasticity_result.plot_stress()
-
-# # # elasticity_result.plot_displacement_per_node_x()
-# # # elasticity_result.plot_displacement_per_node_y()
-# # elasticity_result.plot_displacement_per_node_xy()
+phase1, phase2, phase3 = 3, 3, 3
+elasticity_modulus, poisson_ratio, thickness, mass_density = 210*1e9, 0.25, 1, 7860 #acier
 
 
-# # # %%
+elements_phase1 = []
+for i in range(3):
+    element1 = mesh.TriangularElement2D([mesh.Node2D(i,0), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    element2 = mesh.TriangularElement2D([mesh.Node2D(i+1,1), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    elements_phase1.extend([element1, element2])
 
-# # b = 1
-# # I = ((b**3)*thickness)/12 # I = (b*(thickness**3) + thickness*(b**3))/12
-# # fleche_y = (load*10**3)/(3*elasticity_modulus*I)
+solid_elments2d = [fe.elements.ElasticityTriangularElement2D(
+    element, elasticity_modulus, poisson_ratio, mass_density, thickness) for element in elements_phase1]
+group_phase1 = mesh.ElementsGroup(solid_elments2d, 'phase1')
 
-# # elasticity_result.displacement_vectors_per_node[283]
+
+elements_phase2 = []
+for i in range(phase1, phase1+phase2):
+    element1 = mesh.TriangularElement2D([mesh.Node2D(i,0), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    element2 = mesh.TriangularElement2D([mesh.Node2D(i+1,1), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    elements_phase2.extend([element1, element2])
+
+solid_elments2d = [fe.elements.ElasticityTriangularElement2D(
+    element, elasticity_modulus, poisson_ratio, mass_density, thickness) for element in elements_phase2]
+group_phase2 = mesh.ElementsGroup(solid_elments2d, 'phase2')
+
+
+elements_phase3 = []
+for i in range(phase1+phase2, phase1+phase2+phase3):
+    element1 = mesh.TriangularElement2D([mesh.Node2D(i,0), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    element2 = mesh.TriangularElement2D([mesh.Node2D(i+1,1), mesh.Node2D(i+1,0), mesh.Node2D(i,1)])
+    elements_phase3.extend([element1, element2])
+
+solid_elments2d = [fe.elements.ElasticityTriangularElement2D(
+    element, elasticity_modulus, poisson_ratio, mass_density, thickness) for element in elements_phase3]
+group_phase3 = mesh.ElementsGroup(solid_elments2d, 'phase3')
+
+
+mesh_fe = mesh.Mesh([group_phase1, group_phase2, group_phase3])
+ax = mesh_fe.plot()
+ax.set_title("Initial Mesh")
+
+# %% Loads/Conditions
+
+load = -10000000
+application_index = mesh_fe.node_to_index[mesh.Node2D(9,1)]
+node_loads = [fe.loads.NodeLoad(mesh_fe.nodes[application_index], load, 2)]
+
+application_indices = find_points_with_x(nodes=mesh_fe.nodes, x=0)
+node_boundary_conditions = []
+for index in application_indices:
+    node_boundary_conditions.extend([finite_elements.conditions.NodeBoundaryCondition(mesh_fe.nodes[index], 0, 1),
+                                finite_elements.conditions.NodeBoundaryCondition(mesh_fe.nodes[index], 0, 2)])
+
+#%% Analysis: plane_strain
+
+plane_strain, plane_stress = True, False
+
+#%% Analysis: plane_stress
+
+# plane_strain, plane_stress = False, True
+
+# %% Analysis
+
+analysis = fe.analysis.FiniteElementAnalysis(mesh_fe, [], node_loads, [], [], node_boundary_conditions, [],
+                                             plane_strain=plane_strain, plane_stress=plane_stress)
+
+# %% Results
+
+m = analysis.create_matrix()
+
+results = analysis.solve()
+
+elasticity_result = fe.results.ElasticityResults2D(results.mesh, results.result_vector, analysis.plane_strain, analysis.plane_stress)
+
+# %% Plots
+
+elasticity_result.plot_deformed_mesh(amplitude=10)
+elasticity_result.plot_displacement_vectors_per_node(amplitude=0.2)
+
+elasticity_result.plot_displacement_per_node_xy(amplitude=10)
+
+# elasticity_result.plot_strain()
+# elasticity_result.plot_stress()
