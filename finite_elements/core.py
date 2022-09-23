caaa9bbf25609987594300d4c2f96955feb33875
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-09-23 12:55:15 +0200

    Divide electromag.py into small thematic files + Updates

---
+++ finite_elements/core.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Library: finite_elements (core.py)
+"""
+
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+import math
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as npy
+import matplotlib.tri as mtri
+# import volmdlr
+# import volmdlr.mesh as vmmesh
+# from scipy import sparse
+# from scipy import linalg
+# import time
+from dessia_common import DessiaObject
+# from typing import TypeVar, List, Tuple
+
+
+cdict = {'red':  [(0.0, 0.0, 0.0),
+                   (1.0, 1.0, 1.0)],
+         'green': [(0.0, 0.0, 0.0),
+                   (1.0, 0.0, 0.0)],
+         'blue':  [(0.0, 1.0, 1.0),
+                   (1.0, 0.0, 0.0)]}
+
+blue_red = LinearSegmentedColormap('BLueRed', cdict)
+
+MU = 4*math.pi*1e-7
+
+
+def get_bmin_bmax(Bs, Bmax=None, Bmin=None):
+    """
+
+    :param Bs: DESCRIPTION
+    :type Bs: TYPE
+    :param Bmax: DESCRIPTION, defaults to None
+    :type Bmax: TYPE, optional
+    :param Bmin: DESCRIPTION, defaults to None
+    :type Bmin: TYPE, optional
+
+    :return: DESCRIPTION
+    :rtype: TYPE
+    """
+
+    if Bmax is None and Bmin is None:
+        B_max, B_min = max(Bs), min(Bs)
+    elif Bmax is not None and Bmin is None:
+        B_max, B_min = Bmax, min(Bs)
+    elif Bmax is None and Bmin is not None:
+        B_max, B_min = max(Bs), Bmin
+    else:
+        B_max, B_min = Bmax, Bmin
+
+    return B_max, B_min
+
+def get_colors(Bs, B_max=None, B_min=None):
+    """
+
+    :param Bs: DESCRIPTION
+    :type Bs: TYPE
+    :param B_max: DESCRIPTION, defaults to None
+    :type B_max: TYPE, optional
+    :param B_min: DESCRIPTION, defaults to None
+    :type B_min: TYPE, optional
+
+    :return: DESCRIPTION
+    :rtype: TYPE
+    """
+
+    color_map = ((0,0,1), (1,0,0))
+
+    B_to_color = {}
+    for B in Bs:
+        if B > B_max:
+            x = 1
+        else:
+            x = (B - B_min) / (B_max - B_min)
+
+        color = (color_map[0][0]-(color_map[0][0]-color_map[1][0])*x,
+                 color_map[0][1]-(color_map[0][1]-color_map[1][1])*x,
+                 color_map[0][2]-(color_map[0][2]-color_map[1][2])*x)
+        B_to_color[B] = color
+
+    return B_to_color
+
+def global_matrix_positions(dimension, nodes_number):
+    """
+
+    :param dimension: DESCRIPTION
+    :type dimension: TYPE
+    :param nodes_number: DESCRIPTION
+    :type nodes_number: TYPE
+
+    :return: DESCRIPTION
+    :rtype: TYPE
+    """
+
+    positions = {}
+    count = 0
+    for i in range(nodes_number):
+        for j in range(dimension):
+            positions[(i, j+1)] = count
+            count += 1
+
+    return positions
+
+def get_triangulation(mesh):
+    """
+
+    :param mesh: DESCRIPTION
+    :type mesh: TYPE
+
+    :return: DESCRIPTION
+    :rtype: TYPE
+    """
+
+    x_list, y_list = [], []
+    for node in mesh.nodes:
+        x_list.append(node[0])
+        y_list.append(node[1])
+    triangles = []
+    for group in mesh.elements_groups:
+        for element in group.elements:
+            triangles.append([mesh.node_to_index[element.points[0]],
+                              mesh.node_to_index[element.points[1]],
+                              mesh.node_to_index[element.points[2]]])
+
+    x = npy.asarray(x_list)
+    y = npy.asarray(y_list)
+    triang = mtri.Triangulation(x, y, triangles)
+
+    return triang
---
+++ finite_elements/loads.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module containing objects related to different loads types
+"""
+
+from typing import List
+import volmdlr as vm
+import volmdlr.mesh as vmmesh
+from dessia_common import DessiaObject
+
+
+class ElementsLoad(DessiaObject):
+    """
+    Sets a load on the selected elements by imposing a source value for the \
+    current density vector J. Each element creates a source of the input value.
+
+    :param elements: The triangular elements.
+    :type elements: List of volmdlr.TriangularElements objects
+    :param value: Set the elements' current density vector J value.
+    :type value: float
+    """
+    def __init__(self, elements: List[vmmesh.TriangularElement],
+                 value: float,
+                 dimension: int):
+        self.elements = elements
+        self.value = value
+        self.dimension = dimension
+
+        self.value_per_element = []
+        total_area = sum([elem.area for elem in self.elements])
+        for element in self.elements:
+            self.value_per_element.append(value * element.area/total_area)
+        DessiaObject.__init__(self, name='')
+
+
+class ElementLoad(DessiaObject):
+    """
+    Sets a load on the selected elements by imposing a source value for the \
+    current density vector J. Each element creates a source of the input value.
+
+    :param elements: The triangular elements.
+    :type elements: List of volmdlr.TriangularElements objects
+    :param value: Set the elements' current density vector J value.
+    :type value: float
+    """
+    def __init__(self, element: vmmesh.TriangularElement,
+                 value: float,
+                 dimension: int):
+        self.element = element
+        self.value = value
+        self.dimension = dimension
+
+        DessiaObject.__init__(self, name='')
+
+class EdgeLoad(DessiaObject):
+    """
+    """
+    def __init__(self, edge, value, dimension):
+        self.edge = edge
+        self.value = value
+        self.dimension = dimension
+
+        DessiaObject.__init__(self, name='')
+
+
+class NodeLoad(DessiaObject):
+    """
+    Forces the value of the vector potential A at a node. To set a magnetic wall \
+    the value of the vector potential has to be set to A.
+
+    :param node: The node.
+    :type node: volmdlr.Point2D object
+    :param value: Set the node's vector potential A value.
+    :type value: float
+    """
+    def __init__(self, node: vm.Point2D, value: float, dimension):
+        self.node = node
+        self.value = value
+        self.dimension = dimension
+
+        DessiaObject.__init__(self, name='')
+
+    def c_matrix(self):
+        """
+        """
+        return ()
+
+    def source_c_matrix(self):
+        """
+        """
+        return self.value
+
+
+class MagnetLoad(DessiaObject):
+    """
+    Sets a load on the selected elements by imposing a source value for the \
+    magnetization vector M. Each element creates a source of the input value.
+
+    :param elements: The triangular elements.
+    :type elements: List of volmdlr.TriangularElements
+    :param non_contour_nodes: Specify the nodes that are not part of the magnet's contour.
+    :type non_contour_nodes: List of volmdlr.Point2D objects
+    :param magnetization_vector: Set the elements' magnetization vector M.
+    :type magnetization_vector: volmdlr.Vector2D object
+    """
+    def __init__(self, elements: List[vmmesh.TriangularElement],
+                 non_contour_nodes: List[vm.Point2D],
+                 magnetization_vector: vm.Vector2D):
+        self.elements = elements
+        self.non_contour_nodes = non_contour_nodes
+        self.magnetization_vector = magnetization_vector
+
+        self.element_magnetization_vector = magnetization_vector / len(elements)
+
+        DessiaObject.__init__(self, name='')
+
+    def contour_linear_elements(self):
+        """
+        """
+
+        linear_elements_count = {}
+        for element in self.elements:
+            for linear_element in element.linear_elements:
+                if linear_element not in linear_elements_count:
+                    linear_elements_count[linear_element] = 1
+                else:
+                    linear_elements_count[linear_element] += 1
+        contour_linear_elements = []
+        for linear_element, count in linear_elements_count.items():
+            if count == 1 \
+             and (linear_element.points[0] not in self.non_contour_nodes \
+             or linear_element.points[1] not in self.non_contour_nodes):
+                contour_linear_elements.append(linear_element)
+        return contour_linear_elements
