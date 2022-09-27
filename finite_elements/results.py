39968fb22a1f64c8a6b88c84218030eca477f6cf
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-09-27 09:42:24 +0200

    results.py (ElasticityResults3D)

--- finite_elements/results.py
+++ finite_elements/results.py
@@ -853,3 +853,198 @@ class ElasticityResults2D(ElasticityResults):
                 shear_stress_xy[element] = stress[element][2]
 
         return shear_stress_xy
+
+
+class ElasticityResults3D(ElasticityResults):
+    # _standalone_in_db = True
+    # _non_serializable_attributes = []
+    # _non_eq_attributes = ['name']
+    # _non_hash_attributes = ['name']
+    # _generic_eq = True
+    # def __init__(self, mesh: vmmesh.Mesh, result_vector: List[float]):
+    #     self.mesh = mesh
+    #     self.result_vector = result_vector
+
+    #     self.displacement_vectors_per_node = self._displacement_vectors_per_node()
+    #     self.displacements_per_element = self._displacements_per_element()
+    #     self.strain, self.stress = self._strain_stress_per_element()
+    #     self.deformed_nodes = self._deformed_nodes()
+    #     self.deformed_mesh = self._deformed_mesh()
+
+    #     Result.__init__(self, mesh, result_vector)
+
+    def axial_strain_x(self):
+
+        strain = self.strain
+        axial_strain_x = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_strain_x[element] = strain[element][0]
+
+        return axial_strain_x
+
+    def axial_strain_y(self):
+
+        strain = self.strain
+        axial_strain_y = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_strain_y[element] = strain[element][1]
+
+        return axial_strain_y
+
+    def axial_strain_z(self):
+
+        strain = self.strain
+        axial_strain_z = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_strain_z[element] = strain[element][2]
+
+        return axial_strain_z
+
+    def axial_stress_x(self):
+
+        stress = self.stress
+        axial_stress_x = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_stress_x[element] = stress[element][0]
+
+        return axial_stress_x
+
+    def axial_stress_y(self):
+
+        stress = self.stress
+        axial_stress_y = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_stress_y[element] = stress[element][1]
+
+        return axial_stress_y
+
+    def axial_stress_z(self):
+
+        stress = self.stress
+        axial_stress_z = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                axial_stress_z[element] = stress[element][2]
+
+        return axial_stress_z
+
+    def displacement_per_node_z(self):
+
+        return [displacement[2] for displacement in self.displacement_vectors_per_node]
+
+    # def plot_axial_strain_z(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='axial_strain_z', ax=ax, fig=fig)
+
+    # def plot_axial_stress_z(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='axial_stress_z', ax=ax, fig=fig)
+
+    # def plot_displacement_per_node_z(self, ax=None):
+
+    #     return self.plot_displacements(displacement_name='displacement_per_node_z', ax=ax)
+
+    # def plot_shear_strain_yz(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='shear_strain_yz', ax=ax, fig=fig)
+
+    # def plot_shear_strain_zx(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='shear_strain_zx', ax=ax, fig=fig)
+
+    # def plot_shear_stress_yz(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='shear_stress_yz', ax=ax, fig=fig)
+
+    # def plot_shear_stress_zx(self, ax=None, fig=None):
+
+    #     return self.plot_constraints(constraint_name='shear_stress_zx', ax=ax, fig=fig)
+
+    # def plot_strain(self, axs=None, fig=None, row=1):
+    #     if fig is None:
+    #         fig = plt.figure()
+
+    #     plot_names = ['plot_axial_strain_x', 'plot_axial_strain_y', 'plot_shear_strain_xy',
+    #                   'plot_axial_strain_z', 'plot_shear_strain_yz', 'plot_shear_strain_zx']
+    #     axs = []
+    #     for i, name in enumerate(plot_names):
+    #         axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
+
+    #     return axs
+
+    # def plot_stress(self, axs=None, fig=None, row=1):
+    #     if fig is None:
+    #         fig = plt.figure()
+
+    #     plot_names = ['plot_axial_stress_x', 'plot_axial_stress_y', 'plot_shear_stress_xy',
+    #                   'plot_axial_stress_z', 'plot_shear_stress_yz', 'plot_shear_stress_zx']
+    #     axs = []
+    #     for i, name in enumerate(plot_names):
+    #         axs.append(getattr(self, name)(ax=plt.subplot(row, 3, i+1), fig=fig))
+
+    #     return axs
+
+    def shear_strain_xy(self):
+
+        strain = self.strain
+        shear_strain_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_strain_xy[element] = strain[element][3]
+
+        return shear_strain_xy
+
+    def shear_strain_yz(self):
+
+        strain = self.strain
+        shear_strain_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_strain_xy[element] = strain[element][4]
+
+        return shear_strain_xy
+
+    def shear_strain_zx(self):
+
+        strain = self.strain
+        shear_strain_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_strain_xy[element] = strain[element][5]
+
+        return shear_strain_xy
+
+    def shear_stress_xy(self):
+
+        stress = self.stress
+        shear_stress_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_stress_xy[element] = stress[element][3]
+
+        return shear_stress_xy
+
+    def shear_stress_yz(self):
+
+        stress = self.stress
+        shear_stress_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_stress_xy[element] = stress[element][4]
+
+        return shear_stress_xy
+
+    def shear_stress_zx(self):
+
+        stress = self.stress
+        shear_stress_xy = {}
+        for group in self.mesh.elements_groups:
+            for element in group.elements:
+                shear_stress_xy[element] = stress[element][5]
+
+        return shear_stress_xy
